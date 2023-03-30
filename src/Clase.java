868379919f5072197f777b890d9e3acfe85b59b3
Tartatamudo <fabianalarcon123@gmail.com>
2023-03-29 23:15:20 -0300

    2Do commit
    me condfundi con la definicion de primo y aqui lo arregle

--- src/Clase.java
+++ src/Clase.java
@@ -25,13 +25,18 @@ public class Clase {
         return entero;
     }
 
-    public static boolean isPrime(int prim){
-
-        if (prim % 2 != 0){
-            return true;
-        }
+    public static boolean isPrime(int numero){
+        if (numero == 0 || numero == 1 || numero == 4) {
             return false;
+                    }
+        for (int i = 2; i < numero / 2; i++) {
+            if (numero % i == 0)
+                return false;
+        }
+
+        return true;
     }
+
     public static void copyVectors(int vec[], ArrayList<Integer> otherVec){
         for (int i = 0; i < vec.length; i++) {
             if (isPrime(vec[i])){
